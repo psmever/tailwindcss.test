10a35cb24a955fbcb148b9f56e80540fd70c260e
psmever <psmever@gmail.com>
2021-01-10 22:58:09 +0900

    [ADD] ChattingComponent.

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,7 @@
 export { default as DefaultPage } from './Default/DefaultPageComponent';
 export { default as MainPage } from './Default/MainPageComponent';
 export { default as TestPage } from './Default/TestPageComponent';
+export { default as Chatting } from './Default/ChattingComponent';
 
 export { default as Navbar } from './element/Navbar';
 export { default as StackedLayouts } from './element/StackedLayouts';
